703cc4feccf2f4ccebbfb479ce952f1553df2559
Dylan Kuo <dylankuo@mac.lan>
2026-01-11 20:58:43 -0500

    Updated gitignore

--- dbt_project/target/compiled/thrive_market/models/staging/stg_customers.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    "CUSTOMERID"::int as customer_id,
-    "EMAIL"::varchar as email,
-    "FIRSTNAME"::varchar as first_name,
-    "BILLINGPOSTCODE"::varchar as billing_post_code
-FROM "thrive"."main"."raw_customers"
--- dbt_project/target/compiled/thrive_market/models/staging/stg_sales.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    "ORDERID"::int as order_id,
-    "CUSTOMERID"::int as customer_id,
-    "PREDISCOUNTGROSSPRODUCTSALES":: decimal(10,2) as pre_discount_gross_product_sales,
-    "ORDERWEIGHT"::decimal(10,2) as order_weight
-FROM "thrive"."main"."raw_sales"
